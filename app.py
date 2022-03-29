b8ad6d05fe9ad1c291cd1966d1298c7433be6b28
Saturn5Vfive <luke.inventor@gmail.com>
2022-03-29 02:00:58 -0500

    ar

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def generate_embed():
     try:
         if not "d" in request.args:
-            return render_template('large_embed.html', url="https://mole-embeds.herokuapp.com/", image='https://tenor.com/view/yes-monty-mole-belly-drum-gif-13805191', description='it doesnt look like you put any data to be associated with the embed?', color="#fad47a", title="Moles Embed Generator")
+            return render_template('large_embed.html', url="https://mole-embeds.herokuapp.com/", image='https://tenor.com/view/yes-monty-mole-belly-drum-gif-13805191.gif', description='it doesnt look like you put any data to be associated with the embed?', color="#fad47a", title="Moles Embed Generator")
         data = request.args.get("d")
     except:
         return Response(status=400)
