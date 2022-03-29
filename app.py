946e06266f9b677a40cdfd3671081503b3434e33
Saturn5Vfive <luke.inventor@gmail.com>
2022-03-29 01:58:04 -0500

    re

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import *
 app = Flask(__name__)
 
 @app.route("/")
-def hey():
-    return 'real'
+def generate_embed():
+    try:
+        if not "d" in request.args:
+            return render_template('large_embed.html', url="https://mole-embeds.herokuapp.com/", image='https://tenor.com/view/yes-monty-mole-belly-drum-gif-13805191', description='it doesnt look like you put any data to be associated with the embed?', color="#fad47a", title="Moles Embed Generator")
+        data = request.args.get("d")
+    except:
+        return Response(status=400)
