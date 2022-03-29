b2625c807a2cab6e877880cbc52ef0a863fbe3d2
Saturn5Vfive <luke.inventor@gmail.com>
2022-03-29 02:07:53 -0500

    ar

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import *
+import json
+import base64
 
 app = Flask(__name__)
 
@@ -7,6 +9,13 @@ def generate_embed():
     try:
         if not "d" in request.args:
             return render_template('large_embed.html', url="https://mole-embeds.herokuapp.com/", image='https://tenor.com/view/yes-monty-mole-belly-drum-gif-13805191.gif', description='it doesnt look like you put any data to be associated with the embed?', color="#fad47a", title="Moles Embed Generator")
-        data = request.args.get("d")
-    except:
+        pdata = request.args.get("d")
+        rdata = base64.b64decode(pdata.encode('ascii')).decode('ascii')
+        data = json.loads(rdata)
+        if data['large'] == True:
+            return render_template('large_embed.html', url=data['url'], image=data['image'], description=data['description'], color=data['color'], title=data['title'])
+        else:
+            return render_template('embed.html', url=data['url'], image=data['image'], description=data['description'], color=data['color'], title=data['title'])
+    except Exception as e:
+        print(e)
         return Response(status=400)
